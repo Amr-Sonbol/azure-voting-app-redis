b51e16248c3099a7ed1202e82031ee823847076d
Amr-Sonbol <76924782+Amr-Sonbol@users.noreply.github.com>
2023-01-01 00:43:23 +0200

    adding jenkinsfile

---
+++ jenkins-tutorial/jenkinsfile.groovy
@@ -0,0 +1,13 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('verify branch') {
+            steps {
+                echo $GIT_BRANCH
+                
+            }
+        }
+        
+    }
+}
